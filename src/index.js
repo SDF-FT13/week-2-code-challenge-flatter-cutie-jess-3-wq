1dc2347359a5cbc87a3d92e38459a0461992fea2
Jesse <jesse.yegon@student.moringaschool.com>
2025-03-24 07:27:22 -0400

    new challenge

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 // Your code here
+
+let my_url = "https://flatacutiesbackend.vercel.app/characters"
+document.addEventListener('DOMContentLoaded', displaysImage)
+function displaysImage(){
+   return  fetch(my_url)
+           .then(res => res.json())
+           .then(data => {
+            data.forEach(animal =>{
+                const page = document.getElementById('character-bar')
+                const spanElement = document.createElement('span')
+                spanElement.innerText = animal.name
+                page.addEventListener("click",()=>spanElement(animal));
+
+                page.appendChild(spanElement);
+                
+            }
+            )
+           })
+          
+           }
+
